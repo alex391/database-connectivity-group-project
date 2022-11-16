1e616f69340e0da3e1b2292744354ea102bcd9aa
adrian marquez <alm3558@g.rit.edu>
2022-11-16 14:58:14 -0500

    Added data for entries,Interests, and User tables

--- StudentFaculty.sql
+++ StudentFaculty.sql
@@ -8,11 +8,7 @@ CREATE TABLE Interests(
   interest_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
   interest VARCHAR(45) NOT NULL,
   PRIMARY KEY (interest_ID)
-<<<<<<< HEAD
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-=======
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
->>>>>>> 6f91f806cdd79f926ccece0618736358aecb5475
 
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users(
@@ -66,8 +62,57 @@ UserID INT UNSIGNED NOT NULL,
  CONSTRAINT Entries_UserID_FK   FOREIGN KEY (UserID)    
        REFERENCES Users(UserID)
             ON DELETE CASCADE
-            ON UPDATE CASCADE
+            ON UPDATE CASCADEgit 
 )ENGINE=InnoDB DEFAULT CHARSET= utf8mb4;
 
 
 
+INSERT INTO Interests VALUES (1,'Java');
+INSERT INTO Interests VALUES (2,'Anthropology');
+INSERT INTO Interests VALUES (3,'Ethics in Computing');
+INSERT INTO Interests VALUES (4,'Calculus');
+INSERT INTO Interests VALUES (5,'Mobile Design');
+INSERT INTO Interests VALUES (6,'Python');
+INSERT INTO Interests VALUES (7,'Biochemical Engineering');
+INSERT INTO Interests VALUES (8,'Biology');
+INSERT INTO Interests VALUES (9,'Art');
+INSERT INTO Interests VALUES (10,'Film and Animation');
+
+
+
+INSERT INTO Users VALUES(100,'SUser1','SPass1','Student',1); 
+INSERT INTO Users VALUES(100,'SUser1','SPass1','Student',3); 
+INSERT INTO Users VALUES(101,'SUser2','SPass2','Student',2); 
+INSERT INTO Users VALUES(102,'SUser3','SPass3','Student',2); 
+INSERT INTO Users VALUES(103,'SUser4','SPass4','Student',1);  
+INSERT INTO Users VALUES(105,'SUser2','SPass2','Student',2); 
+
+INSERT INTO Users VALUES(100,'FUser1','FPass1','Faculty',1); 
+INSERT INTO Users VALUES(100,'FUser1','FPass1','Faculty',3); 
+INSERT INTO Users VALUES(101,'FUser2','FPass2','Faculty',2);
+INSERT INTO Users VALUES(102,'FUser3','FPass3','Faculty',7); 
+INSERT INTO Users VALUES(103,'FUser4','FPass4','Faculty',2); 
+INSERT INTO Users VALUES(104,'FUser5','FPass5','Faculty',7); 
+INSERT INTO Users VALUES(105,'FUser6','FPass6','Faculty',1); 
+INSERT INTO Users VALUES(106,'FUser7','FPass7','Faculty',9); 
+
+
+
+INSERT INTO Entries VALUES('Jim','Habermas','All about Java',100);
+INSERT INTO Entries VALUES('Jim','Habermas','Is it ethical to restirict internet access in low income families',100);
+
+INSERT INTO Entries VALUES('Jim','Habermas','Cultural groups around the world',101);
+INSERT INTO Entries VALUES('Jim','Habermas','All about biochemical Engineering',102);
+
+INSERT INTO Entries VALUES('Jim','Habermas','Cultural Melting Pots',103);
+INSERT INTO Entries VALUES('Jim','Habermas','The specifics of biochemical engineering',104);
+
+INSERT INTO Entries VALUES('Jim','Habermas','Java vs C++',105);
+INSERT INTO Entries VALUES('Jim','Habermas','About abstract Art',106);
+
+
+
+firstName VARCHAR(45) NOT NULL,
+lastName VARCHAR(45) NOT NULL,
+topicTitle VARCHAR(45) NOT NULL,
+UserID INT UNSIGNED NOT NULL,
